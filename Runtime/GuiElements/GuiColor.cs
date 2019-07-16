dba906c11f2cd3ef00843479749d2740b2c3b04c
Sunrise Hirame <benhard.miettinen@gmail.com>
2019-07-16 14:28:38 +0300

    Added slider support to GuiColor

--- Runtime/GuiElements/GuiColor.cs
+++ Runtime/GuiElements/GuiColor.cs
@@ -9,38 +9,41 @@ namespace TwistedArk.DevelopmentConsole.Runtime
         private string hexColor;
 
         private string fieldText;
+        private bool useAlpha;
+        private bool showSliders;
         
-        public GuiColor (string label, Action<Color> valueChanged, in Color color) : base (label, valueChanged)
+        public GuiColor (string label, Action<Color> valueChanged, Color color, 
+            bool useAlpha = false, bool showSliders = false) 
+        : base (label, valueChanged)
         {
+            if (!useAlpha)
+                color.a = 1;
+            
+            this.useAlpha = useAlpha;
+            this.showSliders = showSliders;
+            
             this.color = color;
-            hexColor = $"#{ColorUtility.ToHtmlStringRGBA (color)}";
+            hexColor = $"{ColorUtility.ToHtmlStringRGBA (color)}";
             fieldText = hexColor;
         }
 
-        public GuiColor (string label, Action<Color> valueChanged, in string hexColor) : base (label, valueChanged)
-        {
-            if (ColorUtility.TryParseHtmlString (hexColor, out color))
-            {
-                this.hexColor = $"#{hexColor}";
-                fieldText = this.hexColor;
-            }
-            else
-            {
-                color = Color.white;
-                fieldText = "#FFFFFFFF";
-                this.hexColor = fieldText;
-            }
-        }
-        
         public override void Draw (in Rect rect)
         {
-            var contentRect = DrawPrefixLabel (rect);
+            var lineRect = new Rect (rect.x, rect.y, rect.width, 16);
+            var contentRect = DrawPrefixLabel (lineRect);
             
             PushGuiColor (color);
-            // max: FFFFFFFF
-            fieldText = GUI.TextField (contentRect, fieldText, 9);
+            // max: #FFFFFF(FF)
+            fieldText = GUI.TextField (contentRect, fieldText, useAlpha ? 9 : 7);
             PopGuiColor ();
 
+            if (!fieldText.StartsWith ("#"))
+                fieldText = $"#{fieldText}";
+
+            lineRect.y += 16;
+            if (showSliders)
+                DrawSliders (lineRect);
+            
             if (string.Compare (fieldText, hexColor, StringComparison.Ordinal) == 0)
                 return;
             
@@ -52,10 +55,35 @@ namespace TwistedArk.DevelopmentConsole.Runtime
             
             valueChanged?.Invoke (color);
         }
+        
+        private void DrawSliders (Rect lineRect)
+        {
+            var newColor = color;
+            newColor.r = GUI.HorizontalSlider (lineRect, color.r, 0, 1);
 
+            lineRect.y += 16;
+            newColor.g = GUI.HorizontalSlider (lineRect, color.g, 0, 1);
+
+            lineRect.y += 16;
+            newColor.b = GUI.HorizontalSlider (lineRect, color.b, 0, 1);
+
+            if (useAlpha)
+            {
+                lineRect.y += 16;
+                newColor.a = GUI.HorizontalSlider (lineRect, color.a, 0, 1);
+            }
+
+            if (color != newColor)
+            {
+                color = newColor;
+                fieldText = ColorUtility.ToHtmlStringRGBA (color);
+                hexColor = fieldText;
+            }
+        }
+        
         public override float GetHeight ()
         {
-            return 16;
+            return showSliders ? 16 * (useAlpha ? 5 : 4) : 16;
         }
 
         public override void Dispose ()
