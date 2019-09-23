2f8eac7e55e8e85cc57afca6c54d281aad63e12e
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-09-23 22:42:16 +0300

    Added Toggle

---
+++ Runtime/GuiElements/GuiToggle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace TwistedArk.DevelopmentConsole.Runtime
+{
+    public class GuiToggle : GuiElement<bool>
+    {
+        private bool currentValue;
+        
+        public GuiToggle (string label, bool startValue, Action<bool> valueChanged) : 
+            base (label, valueChanged)
+        {
+            currentValue = startValue;
+        }
+
+        public override void Draw (in Rect rect)
+        {
+            var contentRect = DrawPrefixLabel (rect);
+            
+            var oldValue = currentValue;
+            currentValue = GUI.Toggle (contentRect, currentValue, GUIContent.none);
+
+            if (currentValue != oldValue)
+            {
+                valueChanged?.Invoke (currentValue);
+            }
+        }
+    }
+
+}
